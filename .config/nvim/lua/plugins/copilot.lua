8c7c917c699dbdbe762eb829daf8164f3ad40cb8
richiedevr <richie.rosario@protonmail.com>
2024-03-09 19:41:23 -0500

    replaced copilot with codeium because im poor.jk but kinda not jk

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,9 +1,12 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot",
-  opts = {
-    suggestion = { enabled = true },
-    panel = { enabled = true },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+    { "zbirenbaum/copilot.lua", enabled = false },
   },
 }
